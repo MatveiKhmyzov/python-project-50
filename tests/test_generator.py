b6d35c5f632e710b04c5dc6697613ee12e49de7f
Matvei Khmyzov <matw.hmizov@yandex.ru>
2023-01-10 23:06:26 +0300

    Fixed tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -28,20 +28,29 @@ def test_cli(capsys):
 
 
 def test_json_files():
-    filepath1 = random.choice(filepaths1)
-    filepath2 = random.choice(filepaths2)
-    format_name = random.choice(format_names)
-    correct_answer_path = get_correct_answer_path(format_name)
-    correct_answer = open(correct_answer_path).read()
-    if "stylish" in format_name:
-        assert generate_diff(filepath1, filepath2, format_name) == correct_answer
-    elif "plain" in format_name:
-        assert generate_diff(filepath1, filepath2, format_name) == correct_answer
-    elif "json" in format_name:
-        assert generate_diff(filepath1, filepath2, format_name) == correct_answer
+    json_filepath1 = "tests/fixtures/bigfile1.json"
+    json_filepath2 = "tests/fixtures/bigfile2.json"
+    stylish_result = open("tests/fixtures/stylish_view_result.txt").read()
+    plain_result = open("tests/fixtures/plain_view_result.txt").read()
+    json_result = open("tests/fixtures/json_view_result.txt").read()
+    assert generate_diff(json_filepath1, json_filepath2, "stylish") == stylish_result
+    assert generate_diff(json_filepath1, json_filepath2, "plain") == plain_result
+    assert generate_diff(json_filepath1, json_filepath2, "json") == json_result
+
+
+def test_yaml_files():
+    yaml_filepath1 = "tests/fixtures/bigfile1.yaml"
+    yaml_filepath2 = "tests/fixtures/bigfile2.yaml"
+    stylish_result = open("tests/fixtures/stylish_view_result.txt").read()
+    plain_result = open("tests/fixtures/plain_view_result.txt").read()
+    json_result = open("tests/fixtures/json_view_result.txt").read()
+    assert generate_diff(yaml_filepath1, yaml_filepath2, "stylish") == stylish_result
+    assert generate_diff(yaml_filepath1, yaml_filepath2, "plain") == plain_result
+    assert generate_diff(yaml_filepath1, yaml_filepath2, "json") == json_result
 
 
 def get_correct_answer_path(format_name):
     for elem in result_filepaths:
         if format_name in elem:
             return elem
+    
